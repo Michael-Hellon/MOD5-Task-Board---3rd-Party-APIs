d4e53e0c19b5bc9c5301b8ef733bbdddfc5cf6be
Michael Hellon <kansasboy308@gmail.com>
2024-05-02 14:28:54 -0500

    added taskID and nextId fucntion Changes to be committed: modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,46 @@ const taskDescription =$('task-description');
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
-    id: generateTaskId(), // 
-    title: taskTitle,
-    description: taskDescription,
-    dueDate: projectDate,
-};
+// Retrieve tasks from localStorage and parse the JSON to an array.
+// If there are no tasks in localStorage, initialize an empty array and return it.
+function readTaskFromStorage() {
+  let taskList = JSON.parse(localStorage.getItem('tasks'));
+  if(!tasklist) {
+    taskList = []1;
+  }
+  return taskList;
+}
 
-// Todo: create a function to create a task card
+// a function to generate a unique task id
+function readNextIdFromStorage() {
+  let nextId = JSON.parse(localStorage.getItem('nextId'));
+  if(!nextId) {
+    nextId = 1;
+  } else {
+    nextId++; 
+  }
+  localStorage.setItem('nextId', JSON.stringify(nextId));
+  return nextId;
+}
+
+//  a function that accepts an array of tasks, stringifys them, and saves them in localStorage.
+function saveTasksToStorage(taskList) {
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+}
+
+//  a function to create a task card
 function createTaskCard(task) {
-  // create a task card
-  const taskCard = $('<div>').addClass('card task-card draggable my-3').attr('data-task-id', task.id);
-// TODO: Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
+// create a task card
+const taskCard = $('<div>').addClass('card task-card draggable my-3').attr('data-task-id', task.id);
+// Creates a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
 const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
-// TODO: Create a new card body element and add the class `card-body`.
+// Creates a new card body element and add the class `card-body`.
 const cardBody = $('<div>').addClass('card-body');
-// TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
+// Creates a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
 const cardDescription = $('<p>').addClass('card-text').text(task.description);
-// TODO: Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
+// Creates a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
 const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
-// TODO: Create a new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
+// Creates a new button element and add the classes `btn`, `btn-danger`, and `delete`. Also set the text of the button to "Delete" and add a `data-project-id` attribute and set it to the project id.
 const cardDeleteBtn = $('<button>').addClass('btn btn-danger delete').text('Delete').attr('data-task-id', task.id);
 // add a click event to deleteBtn
 cardDeleteBtn.on('click', handleDeleteProject);
